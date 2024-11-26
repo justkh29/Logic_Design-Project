05d343a41e209639f6da2cc00371e0faa42cf148
Hung Ta <khanghung711@gmail.com>
2024-11-26 20:16:44 +0700

    Update new led module

--- led/led.ino
+++ led/led.ino
@@ -1,28 +1,8 @@
-#define triggerPin1 33
-#define echoPin1 32
-
-#define triggerPin2 0
-#define echoPin2 4
-
-int cm1 = 0;
-int cm2 = 0;
-int duration1 = 50;
-int duration2 = 30;
-int count1 = 0;
-int count2 = 0;
-int status1 = 0;
-int status2 = 0;
-
-void setup()
-{
-  Serial.begin(115200);
-  pinMode(5, OUTPUT);
-  pinMode(18, OUTPUT);
-  pinMode(triggerPin1, OUTPUT);
-  pinMode(echoPin1, INPUT);
-  pinMode(triggerPin2, OUTPUT);
-  pinMode(echoPin2, INPUT);
-}
+// Define multiple sensor parameters
+int cm1 = 0, cm2 = 0;
+int status1 = 0, status2 = 0;
+int duration1 = 5, duration2 = 4; 
+long lastTime1 = 0, lastTime2 = 0;
 
 long readDistance(int triggerPin, int echoPin)
 {
@@ -36,42 +16,55 @@ long readDistance(int triggerPin, int echoPin)
   return pulseIn(echoPin, HIGH);
 }
 
-void ledControl(int& cm, int range, int triggerPin, int echoPin, int& count, int& duration, int& status, int output)
+void ledControl(int &cm, int range, int triggerPin, int echoPin, int duration, int &status, int output, long &lastTime)
 {
+  unsigned long now = millis();
+
   cm = 0.0173 * readDistance(triggerPin, echoPin);
-  Serial.print(cm);
-  Serial.println("cm");
-  if (cm <= range)
+
+  switch (status)
   {
-    status = 1;
+  case 0: // Waiting for object in range
+    if (cm <= range)
+    {
+      digitalWrite(output, HIGH); 
+      lastTime = now;             
+      status = 1;              
+    }
+    break;
+
+  case 1: // Object detected
+    Serial.print(now - lastTime);
+    Serial.print("\n");
+    if (now - lastTime >= duration * 1000) 
+    {
+      status = 2;
+    }
+    break;
+
+  case 2:
+    digitalWrite(output, LOW); 
+    status = 0;               
+    break;
   }
-  switch(status)
-  {
-    case 0:
-      break;
-    case 1:
-      digitalWrite(output, HIGH);
-      if (duration > 0)
-      {
-        count++;
-        if (count >= duration)
-        {
-          status = 2;
-          count = 0;
-          Serial.print("Done. Change status to ");
-          Serial.println(status);
-        }
-      }
-      break;
-    case 2:
-      digitalWrite(output, LOW);
-      break;
-  }
+}
+
+void setup()
+{
+  Serial.begin(9600);
+
+  // Set sensor pins and outputs
+  pinMode(17, INPUT); // Echo 1
+  pinMode(16, OUTPUT); // Trigger 1
+  pinMode(5, OUTPUT); // LED 1
+
+  pinMode(19, INPUT); // Echo 2
+  pinMode(18, OUTPUT); // Trigger 2
+  pinMode(4, OUTPUT); // LED 2
 }
 
 void loop()
 {
-  ledControl(cm1, 5, triggerPin1, echoPin1, count1, duration1, status1, 5);
-  ledControl(cm2, 20, triggerPin2, echoPin2, count2, duration2, status2, 18);
-  delay(100);
+  ledControl(cm1, 20, 16, 17, duration1, status1, 5, lastTime1);
+  ledControl(cm2, 30, 18, 19, duration2, status2, 4, lastTime2);
 }
